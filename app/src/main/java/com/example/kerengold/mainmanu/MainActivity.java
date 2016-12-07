9d18b42e95524990fb3b23ef74e7e39c2fcfe5a8
kerengoldd <kerengold55@gmail.com>
2016-12-07 21:34:34 +0200

    ex 3 cons.layouts

--- app/src/main/java/com/example/kerengold/mainmanu/MainActivity.java
+++ app/src/main/java/com/example/kerengold/mainmanu/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.kerengold.mainmanu;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -52,13 +53,27 @@ public class MainActivity extends AppCompatActivity {
                     startActivity(c);
                 }
 
-
             }
         });
 
 
 
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)  {
+
+        int id = item.getItemId();
+        if ( id == R.id.action_settings){
+            Intent intent = new Intent(this,SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+
+
 
     }
 }
+
---
+++ app/src/test/java/com/example/kerengold/mainmanu/MainActivityTest.java
@@ -0,0 +1,8 @@
+package com.example.kerengold.mainmanu;
+
+/**
+ * Created by KerenGold on 12/7/16.
+ */
+public class MainActivityTest {
+
+}
